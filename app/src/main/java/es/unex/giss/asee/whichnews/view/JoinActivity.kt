2b222504fe460ceadac78c15feb08a2e0e6f5a48
Seslavav <seslavav@alumnos.unex.es>
2023-11-12 20:01:10 +0100

    GB02-29 Desarrollar la funcionalidad de registro de usuario.

--- app/src/main/java/es/unex/giss/asee/whichnews/view/JoinActivity.kt
+++ app/src/main/java/es/unex/giss/asee/whichnews/view/JoinActivity.kt
@@ -6,12 +6,16 @@ import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
+import es.unex.giss.asee.whichnews.data.models.User
+import es.unex.giss.asee.whichnews.database.WhichNewsDatabase
 import es.unex.giss.asee.whichnews.databinding.ActivityJoinBinding
+import es.unex.giss.asee.whichnews.utils.CredentialCheck
 import kotlinx.coroutines.launch
 
 class JoinActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityJoinBinding
+    private lateinit var db: WhichNewsDatabase
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,10 +24,12 @@ class JoinActivity : AppCompatActivity() {
         binding = ActivityJoinBinding.inflate(layoutInflater) //se establece el layout de nuestra activity
         setContentView(binding.root)
 
+        //database instance reference
+        db = WhichNewsDatabase.getInstance(applicationContext)!!
+
         //views initialization and listeners
         setUpUI()
         setUpListeners()
-
     }
 
     private fun setUpUI() {
@@ -31,7 +37,61 @@ class JoinActivity : AppCompatActivity() {
     }
 
     private fun setUpListeners() {
+        with(binding) {
+            //Sign Up Button
+            btSign.setOnClickListener { joinUser() }
 
+        }
 
     }
+
+    private fun navigateToHomeActivity(user: User) {
+        val intent = Intent(this, WelcomeActivity::class.java)
+        intent.putExtra("SIGNUP_USER", user)
+        startActivity(intent)
+    }
+
+    private fun notifyInvalidCredentials(msg: String) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun joinUser() {
+        with(binding) {
+            val check = CredentialCheck.join(
+                etFirstname.text.toString(),
+                etSecondname.text.toString(),
+                etUser.text.toString(),
+                etEmail.text.toString(),
+                etPassword.text.toString(),
+                etRepeatpassword.text.toString()
+            )
+            if (check.fail) notifyInvalidCredentials(check.msg)
+            else {
+                lifecycleScope.launch{
+                    val user = User(
+                        null,
+                        etFirstname.text.toString(),
+                        etSecondname.text.toString(),
+                        etUser.text.toString(),
+                        etEmail.text.toString(),
+                        etPassword.text.toString()
+                    )
+
+                    val id = db?.userDao()?.insert(user)
+
+                    navigateToHomeActivity(
+                        User(
+                            null,
+                            etFirstname.text.toString(),
+                            etSecondname.text.toString(),
+                            etUser.text.toString(),
+                            etEmail.text.toString(),
+                            etPassword.text.toString()
+                        )
+                    )
+                }
+            }
+        }
+    }
+
 }
