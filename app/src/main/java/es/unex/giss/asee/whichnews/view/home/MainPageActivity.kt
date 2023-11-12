ae9764d203bd063e985c3051be9877c6a172ac68
Soledad <mdiaznc@alumnos.unex.es>
2023-11-12 21:10:19 +0100

    GB02-71 Creacíon del algoritmo de filtrado

--- app/src/main/java/es/unex/giss/asee/whichnews/view/home/MainPageActivity.kt
+++ app/src/main/java/es/unex/giss/asee/whichnews/view/home/MainPageActivity.kt
@@ -13,6 +13,7 @@ class MainPageActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainPageBinding
 
+
     private lateinit var newsFragment: NewsFragment
     private lateinit var trendingFragment: TrendingFragment
     private lateinit var favoritesFragment: FavoritesFragment
