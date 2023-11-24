e576e4bfd6072366691d0f798c8ecd10fa2bb922
Seslavav <seslavav@alumnos.unex.es>
2023-11-24 17:56:33 +0100

    GB02-103 Mostrar las noticias más destacadas en la interfaz de usuario

---
+++ app/src/main/java/es/unex/giss/asee/whichnews/view/home/RecentFragment.kt
@@ -0,0 +1,205 @@
+package es.unex.giss.asee.whichnews.view.home
+import android.content.Context
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import es.unex.giss.asee.whichnews.BuildConfig
+import es.unex.giss.asee.whichnews.api.ApiService
+import es.unex.giss.asee.whichnews.api.RetrofitClient
+import es.unex.giss.asee.whichnews.data.models.News
+import es.unex.giss.asee.whichnews.data.models.NewsResponse
+import es.unex.giss.asee.whichnews.database.WhichNewsDatabase
+import es.unex.giss.asee.whichnews.databinding.FragmentRecentBinding
+import es.unex.giss.asee.whichnews.login.UserManager
+import es.unex.giss.asee.whichnews.view.NewsAdapter
+import kotlinx.coroutines.launch
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import java.text.SimpleDateFormat
+import java.util.*
+
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+class RecentFragment : Fragment() {
+
+    private lateinit var emptyMessageTextView: TextView
+
+    private var param1: String? = null
+    private var param2: String? = null
+
+    private lateinit var db: WhichNewsDatabase
+    private var _binding: FragmentRecentBinding?=null
+    private val binding get() = _binding!!
+    private lateinit var adapter: NewsAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Utilizamos View Binding para inflar el diseño
+        _binding = FragmentRecentBinding.inflate(inflater, container, false)
+
+        // TextView para mostrar un mensaje cuando la lista está vacía
+        emptyMessageTextView = binding.emptyMessageTextView
+
+        return binding.root
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        db = WhichNewsDatabase.getInstance(context)!!
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        loadAndSetUpRecentFeed()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        loadAndSetUpRecentFeed()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null // avoid memory leaks
+    }
+
+    companion object {
+        private val dateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+    }
+
+    private fun loadAndSetUpRecentFeed() {
+        // crear el cliente de la API
+        val apiService: ApiService = RetrofitClient.apiService
+        val call: Call<NewsResponse> = apiService.getNews(
+            apiKey = BuildConfig.NEWS_API_KEY,
+            country = "us",
+            category = "general",
+            page = 1,
+            pageSize = 20,
+        )
+
+        call.enqueue(object : Callback<NewsResponse>
+        {
+            // Se ejecuta si recibimos respuesta
+            override fun onResponse(call: Call<NewsResponse>, response: Response<NewsResponse>) {
+                val result: Result<NewsResponse> = RetrofitClient.handleApiResponse(response)
+
+                // Si se produce error en la llamada se imprime
+                if (!result.isSuccess) {
+                    val error = result.exceptionOrNull()
+                    Log.e("RECENT_FRAGMENT", "Error: ${error?.message}")
+                    return
+                }
+
+                Log.e("RECENT_FRAGMENT", "CONEXIÓN A LA API REALIZADA")
+                val newsResponse = result.getOrNull()
+                val newsList: List<News> = newsResponse?.articles ?: emptyList()
+
+                // Obtener la fecha de hoy
+                val today = Calendar.getInstance().time
+
+                // Filtrar las noticias de la última semana
+                val filteredNewsList = newsList.filter { news ->
+                    val newsDate = dateFormat.parse(news.publishedAt)
+                    isWithinLastFiveDays(newsDate, today)
+                }
+                // Log para verificar las fechas filtradas
+                for (news in filteredNewsList) {
+                    Log.e("RECENT_FRAGMENT", "Fecha de publicación: ${news.publishedAt}")
+                }
+                setUpRecyclerView(filteredNewsList)
+            }
+
+            // Se ejecuta si se produce un error de red
+            override fun onFailure(call: Call<NewsResponse>, t: Throwable) {
+                Log.e("RECENT_FRAGMENT", "Error on API call: ${t.message}")
+            }
+        })
+    }
+
+    private fun setUpRecyclerView(newsList: List<News>){
+        // Actualizar el RecyclerView con la lista filtrada
+        adapter = NewsAdapter(
+            newsList,
+            onClick = {
+                toggleLike(it)
+            },
+            context = context
+        )
+
+        with(binding){
+            rvNewsList.layoutManager = LinearLayoutManager(context)
+            rvNewsList.adapter = adapter
+        }
+
+        // Mostrar o ocultar el mensaje cuando la lista está vacía
+        if (newsList.isEmpty()) {
+            emptyMessageTextView.visibility = View.VISIBLE
+        } else {
+            emptyMessageTextView.visibility = View.GONE
+        }
+    }
+
+    // Función para verificar si la noticia ha sido publicada en los últimos cinco días
+    private fun isWithinLastFiveDays(newsDate: Date, currentDate: Date): Boolean {
+        val calNews = Calendar.getInstance().apply { time = newsDate }
+        val calCurrent = Calendar.getInstance().apply { time = currentDate }
+
+        // Comparar las fechas para verificar si están dentro de la última semana
+        return calNews.after(Calendar.getInstance().apply { add(Calendar.DAY_OF_MONTH, -5) }) &&
+                calNews.before(calCurrent)
+    }
+
+    private fun toggleLike(it: News){
+        lifecycleScope.launch {
+            val foundNews = db.newsDao().findNews(it.url ?: "")
+            if(foundNews != null){ // si la encuentro
+                if (foundNews.isFavourite) unsetFavorite(foundNews) else setFavorite(foundNews)
+            }else{
+                if (it.isFavourite) unsetFavorite(it) else setFavorite(it)
+            }
+
+        }
+    }
+
+    private fun setFavorite(news: News){
+        lifecycleScope.launch{
+            if(db.newsDao().findNews(news.url ?: "") != null){ // si existe la noticia
+                db.newsDao().setLike(news.url, true)
+            }else{ // si no existe,
+                news.isFavourite = true
+                UserManager.loadCurrentUser(requireContext())?.userId?.let { userId ->
+                    db.newsDao().insertAndRelate(news, userId)
+                }
+            }
+
+            Toast.makeText(context, "Added to 'My Likes'", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun unsetFavorite(news: News){
+        lifecycleScope.launch {
+            db.newsDao().setLike(news.url, false)
+            Toast.makeText(context, "Removed from 'My Likes'", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
--- app/src/main/java/es/unex/giss/asee/whichnews/view/home/TrendingFragment.kt
+++ app/src/main/java/es/unex/giss/asee/whichnews/view/home/TrendingFragment.kt
@@ -1,60 +1,54 @@
 package es.unex.giss.asee.whichnews.view.home
-
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import es.unex.giss.asee.whichnews.R
+// import es.unex.giss.asee.whichnews.data.AppDatabase
+import es.unex.giss.asee.whichnews.databinding.FragmentNewsBinding
+import es.unex.giss.asee.whichnews.databinding.FragmentTrendingBinding
+import es.unex.giss.asee.whichnews.view.NewsAdapter
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [TrendingFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class TrendingFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: NewsAdapter
+    private lateinit var binding: FragmentTrendingBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_trending, container, false)
+        // Utilizamos View Binding para inflar el diseño
+        binding = FragmentTrendingBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        recyclerView = view.findViewById(R.id.recyclerViewNews)
+        recyclerView.layoutManager = LinearLayoutManager(activity)
+
+        // Obtener datos y actualizar el RecyclerView
+        fetchDataAndUpdateUI()
+
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment TrendingFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            TrendingFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    private fun fetchDataAndUpdateUI() {
+        // Obtener datos de la base de datos local
+        //val newsLikeList = getNewsLikeDataFromDatabase()
+
+        // Puedes manipular los datos según tus necesidades aquí antes de mostrarlos en el RecyclerView
+
+        // Actualizar el RecyclerView con la lista combinada
+        // adapter = NewsAdapter(requireContext(), manipulatedNewsList)
+        //recyclerView.adapter = adapter
     }
+
+    /*private fun getNewsLikeDataFromDatabase(): List<NewsLike> {
+        // Acceder a la base de datos local y obtener la lista de NewsLike
+        // val dao = AppDatabase.getInstance(requireContext()).newsLikeDao()
+        // return dao.getAllNewsLikes()
+    }*/
 }
